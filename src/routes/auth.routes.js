288a5e4156e94a9ffd526ee817799f3c54b97d6b
Nabil Hazmi Nasir <nhazmi135@gmail.com>
2026-03-29 00:08:54 +0800

    working login feature

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,6 @@ import * as ctrl from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", ctrl.register);
+router.post("/login", ctrl.login);
 
 export default router;
