5a84cfabf92fc952894b7635c7019d0fd8206397
ssfau <farhanammar101@gmail.com>
2026-04-02 04:57:22 +0800

    isp unrestrict

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import app from './src/app.js';
 import dotenv from 'dotenv';
 import connectDB from './src/config/db.js';
 dotenv.config();
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Fix for ISP DNS blocking MongoDB SRV lookups
 
 const PORT = process.env.PORT || 3000;
 
